768d12ac67277f08390b75626ed7bcaea83b8ee0
itonik <itonik@me.com>
2020-11-27 23:31:15 +0300

    queues

--- server.py
+++ server.py
@@ -6,19 +6,42 @@ Main server part
 3. Socket server
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 import asyncio
 
+
 app = FastAPI()
+inbound_queue = asyncio.Queue()
+outbound_queue = asyncio.Queue()
 
 
-async def background_print():
-    for i in range(10):
-        print(f'Yo {i}')
-        await asyncio.sleep(1)
+async def ws_to_queue(websocket: WebSocket):
+    while True:
+        data = await websocket.receive_text()
+        print(f'WS -> Q: {data}')
+        await inbound_queue.put(data)
+
+
+async def queue_to_ws(websocket: WebSocket):
+    while True:
+        data = await outbound_queue.get()
+        print(f'Q -> WS: {data}')
+        await websocket.send(data)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    asyncio.create_task(ws_to_queue(websocket))
+    asyncio.create_task(queue_to_ws(websocket))
 
 
 @app.get("/")
 async def root():
-    asyncio.create_task(background_print())
+    # asyncio.create_task(background_print())
     return {"message": "Hello World"}
+
+
+@app.on_event("startup")
+async def startup_event():
+    pass
